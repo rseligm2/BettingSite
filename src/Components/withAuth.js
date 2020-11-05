c60a0b285a4fdd114084703af98782cf8abbfdce
rseligm2 <rseligm2@illinois.edu>
2020-11-05 17:44:50 -0600

    added home page redirect

--- src/Components/withAuth.js
+++ src/Components/withAuth.js
@@ -26,7 +26,7 @@ export default function withAuth(ComponentToProtect) {
         })
         .catch(err => {
           console.error(err);
-          // this.setState({ loading: false, redirect: true });
+          this.setState({ loading: false, redirect: true });
         });
     }
     render() {
